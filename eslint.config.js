df24b2a16f262d75e42bb51832a5b9d6637bcbca
Leonardo <leonardo10sp@gmail.com>
2024-11-24 14:11:08 -0300

    fix: eslint error

--- eslint.config.js
+++ eslint.config.js
@@ -22,7 +22,7 @@ export default [
     rules: {
       "@typescript-eslint/explicit-function-return-type": "warn",
       "@typescript-eslint/no-explicit-any": "warn",
-      singleQuote: true,
+      "singleQuote": ["error", true],
       "@typescript-eslint/no-unused-vars": [
         "warn",
         {
